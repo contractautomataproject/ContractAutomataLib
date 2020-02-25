d7e27f7be2577a4df749445fe02d9f2c5b253188
Davide <Davide@DESKTOP-TUUKUJ4>
2020-02-25 18:30:23 +0100

    restore previous

---
+++ JaMata/src/CA/State.java
@@ -0,0 +1,31 @@
+package CA;
+
+public class State {
+	
+	private boolean accepting;
+	
+	private boolean initial;
+	
+	
+	public State(boolean accepting, boolean initial) {
+		super();
+		this.accepting = accepting;
+		this.initial = initial;
+	}
+	
+	public boolean isAccepting() {
+		return accepting;
+	}
+	
+	public void setAccepting(boolean accepting) {
+		this.accepting = accepting;
+	}
+	
+	public boolean isInitial() {
+		return initial;
+	}
+	
+	public void setInitial(boolean initial) {
+		this.initial = initial;
+	}
+}
