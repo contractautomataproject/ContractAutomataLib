280282df48eda0504a3e259156e086936e5deb98
Davide <Davide@DESKTOP-TUUKUJ4>
2021-06-12 23:20:08 +0200

    removed warning

--- src/contractAutomata/MSCAConverter.java
+++ src/contractAutomata/MSCAConverter.java
@@ -1,7 +1,5 @@
 package contractAutomata;
 
-import java.io.IOException;
-
 public interface MSCAConverter {
 	public MSCA importMSCA(String filename) throws Exception;
 	public void exportMSCA(String filename, MSCA aut) throws Exception;
