8d27a055c9684b531da6cbaea547482a53fc1b07
Davide <Davide@DESKTOP-TUUKUJ4>
2021-06-13 00:01:53 +0200

    removed 2 files

--- src/contractAutomata/JsonConverter.java
+++
@@ -1,8 +0,0 @@
-package contractAutomata;
-
-import java.io.IOException;
-
-public interface JsonConverter {
-	public MSCA importJSON(String path) throws IOException;
-	public void exportJSON(MSCA aut, String path) throws Exception;
-}
--- src/contractAutomata/MSCAConverterR.java
+++
@@ -1,8 +0,0 @@
-package contractAutomata;
-
-import java.io.File;
-
-public interface MSCAConverterR {
-	public MSCA importMSCA(String path) throws Exception;
-	public File exportMSCA(String path, MSCA aut) throws Exception;
-}
