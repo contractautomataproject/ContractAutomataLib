0f73fde0cf11137bde3859155a0d2a4404110ffc
Davide Basile <davide.basile@isti.cnr.it>
2021-09-22 17:08:36 +0200

    Add files via upload

---
+++ doc/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"contractAutomata.automaton"},{"l":"contractAutomata.automaton.label"},{"l":"contractAutomata.automaton.state"},{"l":"contractAutomata.automaton.transition"},{"l":"contractAutomata.converters"},{"l":"contractAutomata.operators"},{"l":"contractAutomata.requirements"},{"l":"family"},{"l":"family.converters"}]
