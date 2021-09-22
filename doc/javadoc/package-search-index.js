02ac905f285c1dc5d659fb760903f03f426efd93
Davide Basile <davide.basile@isti.cnr.it>
2021-09-23 01:00:59 +0200

    Delete doc/javadoc directory

--- doc/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"contractAutomata.automaton"},{"l":"contractAutomata.automaton.label"},{"l":"contractAutomata.automaton.state"},{"l":"contractAutomata.automaton.transition"},{"l":"contractAutomata.converters"},{"l":"contractAutomata.operators"},{"l":"contractAutomata.requirements"},{"l":"family"},{"l":"family.converters"}]
